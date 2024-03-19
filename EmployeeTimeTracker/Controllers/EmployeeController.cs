d00b6930293c73fcfa73dc0eeddad88c78520c79
unknown <babicjovan993@gmail.com>
2024-03-19 18:40:57 +0100

    first commit

---
+++ EmployeeTimeTracker/Controllers/EmployeeController.cs
@@ -0,0 +1,47 @@
+﻿using EmployeeTimeTracker.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace EmployeeTimeTracker.Controllers
+{
+    [Route("employee")]
+    public class EmployeeController : Controller
+    {
+        Uri baseAdress = new Uri("https://rc-vault-fap-live-1.azurewebsites.net/api/gettimeentries?code=vO17RnE8vuzXzPJo5eaLLjXjmRW07law99QTD90zat9FfOQJKKUcgQ==");
+        private readonly HttpClient _client;
+        public EmployeeController()
+        {
+            _client = new HttpClient();
+            _client.BaseAddress = baseAdress;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<Employee> employees = new List<Employee>();
+            HttpResponseMessage response = _client.GetAsync(baseAdress).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                string data = response.Content.ReadAsStringAsync().Result;
+                employees = JsonConvert.DeserializeObject<List<Employee>>(data);
+
+                var groupedEmployees = employees
+                    .Where(e => e.EmployeeName != null) // Filter out entries with null EmployeeName
+                    .GroupBy(e => e.EmployeeName)
+                    .Select(group => new Employee
+                    {
+                        EmployeeName = group.Key,
+                        TotalTimeWorked = Math.Round(group.Sum(e => (e.EndTimeUtc - e.StarTimeUtc).TotalHours), 2)
+                    })
+                    .OrderByDescending(e => e.TotalTimeWorked)
+                    .ToList();
+
+                return View(groupedEmployees);
+            }
+
+            // If request fails, return an empty list
+            return View(employees);
+        }
+    }
+}
